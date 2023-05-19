2db9c0ca71cfb8b4d213f602d2621e065976d5ee
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-18 20:48:18 -0500

    work on coming soon section

--- src/components/ComingSoonContainer/ComingSoonContainer.jsx
+++ src/components/ComingSoonContainer/ComingSoonContainer.jsx
@@ -1,7 +1,21 @@
 import "./ComingSoonContainer.css";
 
 function ComingSoonContainer() {
-  return <div>ComingSoonContainer</div>;
+  return (
+    <section className="comingSoonContainer">
+      <div className="were">{"WE'RE"}</div>
+      <div className="coming">COMING SOON</div>
+      <p>
+        {`Hello fellow shoppers! We're currently building our new fashion store.
+        Add your email below to stay up-to-date with announcements and our
+        launch deals.`}
+      </p>
+      <div>
+        <input placeholder="Email Address" type="email" />
+        <button>{">"}</button>
+      </div>
+    </section>
+  );
 }
 
 export default ComingSoonContainer;
