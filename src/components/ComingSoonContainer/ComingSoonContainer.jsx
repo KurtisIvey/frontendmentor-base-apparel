7ea1e808a034b27971af9ddca028139948df5409
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-19 20:48:47 -0500

    update input error validation

--- src/components/ComingSoonContainer/ComingSoonContainer.jsx
+++ src/components/ComingSoonContainer/ComingSoonContainer.jsx
@@ -1,10 +1,26 @@
 import "./ComingSoonContainer.css";
 import arrow from "../../assets/images/icon-arrow.svg";
+import errIcon from "../../assets/images/icon-error.svg";
 import Title from "../Title/Title";
+import { useState } from "react";
 
 function ComingSoonContainer() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+  const emailRegex = new RegExp(
+    /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/,
+    "gm"
+  );
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const isValidEmail = emailRegex.test(email);
+    if (!isValidEmail) {
+      setError(true);
+    }
+    if (isValidEmail) {
+      setError(false);
+    }
   };
 
   return (
@@ -23,11 +39,15 @@ function ComingSoonContainer() {
       <form className="emailContainer" onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="email"></label>
         <input
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Email Address"
-          type="email"
+          type="text"
           id="email"
           aria-label="email"
+          className={error ? "errorBorder" : ""}
         />
+        {error && <img className="errIcon" src={errIcon} />}
+        {error && <p>Please provide a valid email</p>}
         <button type="submit" className="emailButton" aria-label="Submit">
           <img src={arrow} alt="arrow icon" />
         </button>
